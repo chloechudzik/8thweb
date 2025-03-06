55d7033dcdd1edb3fcf37edfaa2b7e87c22b46b7
chloechudzik <cchudzik@eastsideprep.org>
2025-03-06 11:24:03 -0800

    Add files via upload

---
+++ final/js/script.js
@@ -0,0 +1,9 @@
+alert('This is a test')
+
+function askQuestion() {
+    var p = prompt('How much wood would a woodchuck chuck if a woodchuck would chuck wood?')
+    if (p !=null) {
+        document.getElementById('question').innerHTML = 'Yikes!' + p + ' is a lot of wood!';
+    }
+}
+
